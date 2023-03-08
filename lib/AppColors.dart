dd26d9156ba624185d582a48bf374ba2f6599984
ShellyC6 <57796687+ShellyC6@users.noreply.github.com>
2023-03-08 11:20:44 +0100

    AppColors

---
+++ lib/AppColors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgColor = Color(0xFF1A2025);
+  static const Color contrastColor = Color(0xFF1E262C);
+  static const Color contrastWithTransparency = Color(0xE61E262C);
+  static const Color textColor = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFFAFB8BB);
+  static const Color accentColor = Color(0xFF636AF6);
+  static const Color red = Color(0xFFFF004F);
+  static const Color starColor = Color(0xFFF5A623);
+}
