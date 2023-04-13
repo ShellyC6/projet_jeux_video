fe49127bd43374fd8dbfdfed06c01b7c5ba6ef14
ShellyC6 <57796687+ShellyC6@users.noreply.github.com>
2023-04-13 21:13:32 +0200

    Corrections

--- lib/find_game.dart
+++ lib/find_game.dart
@@ -17,31 +17,7 @@ abstract class FindGame{
       if (response.statusCode == 200) {
         print("Adding $id to the database...");
         final data = jsonDecode(response.body);
-        if(data['$id']['data']['is_free']==true){
-          final data2 = {
-            "id": id,
-            "nom": data['$id']['data']['name'],
-            "editeur": data['$id']['data']['publishers'][0],
-            "prix": '0€',
-            "image": 'https://media.senscritique.com/media/000017048549/source_big/La_Grande_Aventure_LEGO_Le_Jeu_video.jpg',
-            "description": data['$id']['data']['short_description'],
-            "background": data['$id']['data']['background'],
-            "header": data['$id']['data']['header_image'],
-          };
-          await db.collection("Games").add(data2);
-        }else if(data['$id']['data']['price_overview']!=null){
-          final data2 = {
-            "id": id,
-            "nom": data['$id']['data']['name'],
-            "editeur": data['$id']['data']['publishers'][0],
-            "prix": data['$id']['data']['price_overview']['final_formatted'],
-            "image": 'https://media.senscritique.com/media/000017048549/source_big/La_Grande_Aventure_LEGO_Le_Jeu_video.jpg',
-            "description": data['$id']['data']['short_description'],
-            "background": data['$id']['data']['background'],
-            "header": data['$id']['data']['header_image'],
-          };
-          await db.collection("Games").add(data2);
-        }else{
+        /*if(data['$id']['data']['price_overview']==null || data['$id']['data']['price_overview']==null){
           final data2 = {
             "id": id,
             "nom": data['$id']['data']['name'],
@@ -53,6 +29,83 @@ abstract class FindGame{
             "header": data['$id']['data']['header_image'],
           };
           await db.collection("Games").add(data2);
+        } else if(data['$id']['data']['is_free']==true){
+          final data2 = {
+            "id": id,
+            "nom": data['$id']['data']['name'],
+            "editeur": data['$id']['data']['publishers'][0],
+            "prix": '0€',
+            "image": 'https://media.senscritique.com/media/000017048549/source_big/La_Grande_Aventure_LEGO_Le_Jeu_video.jpg',
+            "description": data['$id']['data']['short_description'],
+            "background": data['$id']['data']['background'],
+            "header": data['$id']['data']['header_image'],
+          };
+          await db.collection("Games").add(data2);
+        }else {
+          final data2 = {
+            "id": id,
+            "nom": data['$id']['data']['name'],
+            "editeur": data['$id']['data']['publishers'][0],
+            "prix": data['$id']['data']['price_overview']['final_formatted'],
+            "image": 'https://media.senscritique.com/media/000017048549/source_big/La_Grande_Aventure_LEGO_Le_Jeu_video.jpg',
+            "description": data['$id']['data']['short_description'],
+            "background": data['$id']['data']['background'],
+            "header": data['$id']['data']['header_image'],
+          };
+          await db.collection("Games").add(data2);
+        }*/
+        try{
+          if(data['$id']['data']['is_free']==true){
+            final data2 = {
+              "id": id,
+              "nom": data['$id']['data']['name'],
+              "editeur": data['$id']['data']['publishers'][0],
+              "prix": '0€',
+              "image": 'https://media.senscritique.com/media/000017048549/source_big/La_Grande_Aventure_LEGO_Le_Jeu_video.jpg',
+              "description": data['$id']['data']['short_description'],
+              "background": data['$id']['data']['background'],
+              "header": data['$id']['data']['header_image'],
+            };
+            await db.collection("Games").add(data2);
+          }else {
+            final data2 = {
+              "id": id,
+              "nom": data['$id']['data']['name'],
+              "editeur": data['$id']['data']['publishers'][0],
+              "prix": data['$id']['data']['price_overview']['final_formatted'],
+              "image": 'https://media.senscritique.com/media/000017048549/source_big/La_Grande_Aventure_LEGO_Le_Jeu_video.jpg',
+              "description": data['$id']['data']['short_description'],
+              "background": data['$id']['data']['background'],
+              "header": data['$id']['data']['header_image'],
+            };
+            await db.collection("Games").add(data2);
+          }
+        }catch(e){
+          try{
+            final data2 = {
+              "id": id,
+              "nom": data['$id']['data']['name'],
+              "editeur": data['$id']['data']['publishers'][0],
+              "prix": "Prix inconnu",
+              "image": 'https://media.senscritique.com/media/000017048549/source_big/La_Grande_Aventure_LEGO_Le_Jeu_video.jpg',
+              "description": data['$id']['data']['short_description'],
+              "background": data['$id']['data']['background'],
+              "header": data['$id']['data']['header_image'],
+            };
+            await db.collection("Games").add(data2);
+          }catch(e2){
+            final data2 = {
+              "id": id,
+              "nom": "Erreur",
+              "editeur": "Erreur",
+              "prix": "Prix inconnu",
+              "image": 'https://media.senscritique.com/media/000017048549/source_big/La_Grande_Aventure_LEGO_Le_Jeu_video.jpg',
+              "description": "Erreur",
+              "background": "https://cdn.akamai.steamstatic.com/steam/apps/400/page_bg_generated_v6b.jpg?t=1673562889",
+              "header": "https://tse2.mm.bing.net/th?id=OIP.ctbE91scwm8oq1udQ1-KPgHaF7&pid=Api",
+            };
+            await db.collection("Games").add(data2);
+          }
         }
 
         //print(data['$id']['data']['name']);
