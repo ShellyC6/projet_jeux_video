fe03b42894fec4af53d1e31280b3ca4aa38f8805
ShellyC6 <57796687+ShellyC6@users.noreply.github.com>
2023-04-13 23:16:29 +0200

    Affichage Wishlist

--- lib/screens/wishlist.dart
+++ lib/screens/wishlist.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import '../services/user/user_bloc.dart';
 import '/app_colors.dart';
 import '/widgets/search_bar.dart';
 import '/widgets/apercu.dart';
@@ -12,6 +15,8 @@ class WishlistPage extends StatefulWidget {
 }
 
 class _WishlistPageState extends State<WishlistPage> {
+  final db = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,15 +36,25 @@ class _WishlistPageState extends State<WishlistPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.only(left:10, bottom: 10, right: 10, top:10),
-        child: Column(
-            children: <Widget>[
-              SizedBox(height: 10),
-              Apercu(detail: true, id: 0),
-              SizedBox(height: 10),
-              Apercu(detail: true, id: 0),
-              SizedBox(height: 10),
-              Apercu(detail: true, id: 0),
-            ]
+        child: BlocBuilder<UserBloc, UserState>(
+            builder: (context, state) => StreamBuilder(
+                stream: db.collection("Users")
+                    .where("mail", isEqualTo: state.currentUser.email)
+                    .snapshots(),
+                builder: (context, snapshot){
+                  if(!snapshot.hasData) return const Text("Chargement du jeu en cours...");
+                  return Flexible(
+                    child: ListView.separated(
+                      scrollDirection: Axis.vertical,
+                      itemCount: snapshot.data?.docs.first.get("wishlist").length ?? 0,
+                      itemBuilder: (BuildContext context, int index){
+                        return Apercu(detail: true, id: snapshot.data?.docs.first.get("wishlist")[index]);
+                      },
+                      separatorBuilder: (BuildContext context, int index) => const Divider(color: AppColors.bgColor,),
+                    ),
+                  );
+                }
+            )
         ),
       ),
     );
